3fd06e47e3ae2065fe8f42f4b2d4b3149ad9e87a
Nicky <112414443+Kirkendolled@users.noreply.github.com>
2024-06-13 12:59:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// References to DOM Elements
 const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector("#book");
@@ -5,14 +6,16 @@ const book = document.querySelector("#book");
 const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
 
-// Event Listeners
+// Event Listener
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
-let numOfPapers = 3;
+let numOfPapers = 5;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -27,9 +30,22 @@ function closeBook(isAtBeginning) {
     } else {
         book.style.transform = "translateX(100%)";
     }
-    
     prevBtn.style.transform = "translateX(0px)";
     nextBtn.style.transform = "translateX(0px)";
+}
+
+function resetPages() {
+    paper1.classList.remove("flipped");
+    paper2.classList.remove("flipped");
+    paper3.classList.remove("flipped");
+    paper4.classList.remove("flipped");
+    paper5.classList.remove("flipped");
+
+    paper1.style.zIndex = 5;
+    paper2.style.zIndex = 4;
+    paper3.style.zIndex = 3;
+    paper4.style.zIndex = 2;
+    paper5.style.zIndex = 1;
 }
 
 function goNextPage() {
@@ -47,12 +63,24 @@ function goNextPage() {
             case 3:
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
-                closeBook(false);
+                break;
+            case 4:
+                paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
                 break;
             default:
                 throw new Error("unknown state");
         }
         currentLocation++;
+    } else {
+        // Reset to the beginning
+        closeBook(true);
+        resetPages();
+        currentLocation = 1;
     }
 }
 
@@ -62,21 +90,36 @@ function goPrevPage() {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 5;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 2;
+                paper2.style.zIndex = 4;
                 break;
             case 4:
-                openBook();
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex = 3;
+                break;
+            case 5:
+                paper4.classList.remove("flipped");
+                paper4.style.zIndex = 2;
+                break;
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 1;
                 break;
             default:
                 throw new Error("unknown state");
         }
         currentLocation--;
+    } else {
+        // Reset to the end
+        openBook();
+        paper1.classList.add("flipped");
+        paper2.classList.add("flipped");
+        paper3.classList.add("flipped");
+        paper4.classList.add("flipped");
+        paper5.classList.add("flipped");
+        currentLocation = maxLocation - 1;
     }
 }
-
